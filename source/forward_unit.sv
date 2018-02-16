affb94323fc3292cc616bc2f13870dbf381495c0
Deeptanshu Malik <malikd@purdue.edu>
2018-02-16 03:29:58 -0500

    working on forwarding unit, added LUI case

--- source/forward_unit.sv
+++ source/forward_unit.sv
@@ -20,19 +20,29 @@ module forward_unit (
 
     if(fuif.MEMRegWr) begin
       if(fuif.id_ex_rs == fuif.MEMwsel && fuif.id_ex_rs != 0) begin
-        fuif.forwardselA = 2'd2;
+				if(fuif.MEMlui) begin
+					fuif.forwardselA = 2'd3;
+				end
+				else begin
+        	fuif.forwardselA = 2'd2;
+				end
       end
       else if (fuif.id_ex_rt == fuif.MEMwsel && fuif.id_ex_rt != 0) begin
-        fuif.forwardselB = 2'd2;
+				if(fuif.MEMlui) begin
+					fuif.forwardselB = 2'd3;
+				end
+				else begin
+        	fuif.forwardselB = 2'd2;
+				end
       end
     end
 
     if(fuif.WBRegWr) begin
       if(fuif.id_ex_rs == fuif.WBwsel && fuif.id_ex_rs != 0) begin
-        fuif.forwardselA = 2'd1;
+        fuif.forwardselA = 2'd2;
       end
       else if (fuif.id_ex_rt == fuif.WBwsel && fuif.id_ex_rt != 0) begin
-        fuif.forwardselB = 2'd1;
+        fuif.forwardselB = 2'd2;
       end
 
     end
